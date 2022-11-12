e6cb35ca3304cc827354b2332374713e19a02366
灰原二 <9174159+azarias-jx@user.noreply.gitee.com>
2022-11-12 22:54:49 +0800

    单例模式

--- src/main/java/com/study/design/pay/PayStrategyFactory.java
+++ src/main/java/com/study/design/pay/PayStrategyFactory.java
@@ -2,22 +2,31 @@ package com.study.design.pay;
 
 import com.study.design.pay.PayStrategy;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 /**
  * @description:
  * @author： 灰原二
  * @date: 2022/11/12 10:21
  */
 public class PayStrategyFactory {
+    //单例模式优化工厂返回支付策略
+    private static final Map<String,PayStrategy> strategyMap = new ConcurrentHashMap<>();
 
     public static PayStrategy getPayStrategy(PayStrategyEnum payStrategyEnum){
-        PayStrategy payStrategy = null;
 
-        try {
-            payStrategy = (PayStrategy) payStrategyEnum.getClazz().newInstance();
-        } catch (Exception e) {
-
-            //ignore
+        PayStrategy payStrategy = strategyMap.get(payStrategyEnum.getClazz().getName());
+        if(null == payStrategy){
+            try {
+                payStrategy = (PayStrategy) payStrategyEnum.getClazz().newInstance();
+                strategyMap.put(payStrategy.getClass().getName(),payStrategy);
+            } catch (Exception e) {
+                //ignore
+            }
         }
+
+
 
 
         return payStrategy;
